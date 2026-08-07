4780af5c9f1225af8e8baef8ca44780f6192208f
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2026-08-07 15:39:35 +0530

    SQL CTE (Common Table Expression) Visually Explained | Full Guide WITH Clause

--- SQL CTE (Common Table Expression) Visually Explained  Full Guide WITH Clause.sql
+++ SQL CTE (Common Table Expression) Visually Explained  Full Guide WITH Clause.sql
@@ -55,3 +55,58 @@ ON ccr.CustomerID = c.CustomerID
 LEFT JOIN CTE_Customer_Segments ccs
 ON ccs.CustomerID = c.CustomerID;
 
+/*
+SQL TASK
+Generate a Sequence of Numbers from 1 to 20.
+*/
+WITH Series AS (
+   -- Anchor Query
+   SELECT 1 AS MyNumber
+   UNION ALL
+   -- Recursive Query
+   SELECT MyNumber + 1
+   FROM Series
+   WHERE MyNumber < 20
+)
+-- Main Query
+SELECT *
+FROM Series;
+-- OPTION (MAXRECURSION 10);
+
+
+/*
+SQL TASK
+Show the employee hierarchy by displaying each employee's level with in the organisation.
+*/
+WITH CTE_Emp_Hierarchy AS (
+    -- Anchor Query
+    SELECT EmployeeID,
+           FirstName,
+           ManagerID,
+           1 AS Level
+    FROM Sales.Employees
+    WHERE ManagerID IS NULL
+    UNION ALL
+    -- Recursive Query
+    SELECT e.EmployeeID,
+           e.FirstName,
+           e.ManagerID,
+           Level + 1
+    FROM Sales.Employees AS e
+    INNER JOIN CTE_Emp_Hierarchy AS ceh
+    ON e.ManagerID = ceh.EmployeeID
+)
+
+-- Main Query
+SELECT *
+FROM CTE_Emp_Hierarchy;
+
+
+
+
+
+
+
+
+
+
